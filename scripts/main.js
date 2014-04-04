94cd1f5224540e7d367f373d8bf4f03ff5445d29
Mr Speaker <mrspeaker@gmail.com>
2014-04-04 14:01:12 -0400

    adding cam capture

--- scripts/main.js
+++ scripts/main.js
@@ -87,6 +87,8 @@
 
             this.bindDragDrop();
             this.crunkify();
+
+            this.capture();
         },
 
         bindDragDrop: function () {
@@ -198,6 +200,55 @@
                 id: "png_output",
                 src: canvas.toDataURL()
             }).appendTo(lnk);
+
+        },
+
+        capture: function () {
+            var video = document.querySelector("#videoElement");
+
+            // check for getUserMedia support
+            navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+
+            if (navigator.getUserMedia) {
+                // get webcam feed if available
+                navigator.getUserMedia({video: true}, handleVideo, videoError);
+            }
+
+            function handleVideo(stream) {
+                // if found attach feed to video element
+                video.src = window.URL.createObjectURL(stream);
+            }
+
+            function videoError(e) {
+                // no webcam found - do something
+            }
+            var v,canvas,context,w,h;
+            var imgtag = document.getElementById('imgtag'); // get reference to img tag
+
+            //document.addEventListener('DOMContentLoaded', function(){
+                // when DOM loaded, get canvas 2D context and store width and height of element
+                v = document.getElementById('videoElement');
+                canvas = document.getElementById('canvas');
+                context = canvas.getContext('2d');
+                w = canvas.width;
+                h = canvas.height;
+
+            //},false);
+
+            function draw(v,c,w,h) {
+
+                if(v.paused || v.ended) return false;
+                context.drawImage(v,0,0,w,h);
+
+                var uri = canvas.toDataURL("image/png");
+                imgtag.src = uri;
+            }
+
+            document.getElementById('save').addEventListener('click',function(e){
+
+                draw(v,context,w,h); // when save button is clicked, draw video feed to canvas
+
+            });
 
         }
 
